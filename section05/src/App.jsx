337194e2d881b3e54ab66d6ec3ed7c6a72e9530e
boki <lsb530@naver.com>
2025-01-11 19:03:52 +0900

    State로 상태관리하기 3항연산자 사용, 단방향(훅 사용), App() 다시 호출

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -8,16 +8,26 @@ function App() {
   // 1: fn
 
   // Re-rendering
-  const [state, setState] = useState(0)
+  const [count, setCount] = useState(0)
+  const [light, setLight] = useState("OFF")
 
   return (
     <>
-      <h1>{state}</h1>
-      <button onClick={() => {
-        setState(state + 1)
-      }}>
-        +
-      </button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => setLight(light === 'ON' ? 'OFF' : 'ON')}>
+          {/*전구 끄기/켜기*/}
+          {light === 'ON' ? '끄기' : '켜기'}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => {
+          setCount(count + 1)
+        }}>
+          +
+        </button>
+      </div>
     </>
   )
 }
