afd6ae95225a255f7192aae43225136b2cd08049
boki <lsb530@naver.com>
2025-01-11 18:07:06 +0900

    jsx 규칙

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-      <>
-          <h1>Hello, React</h1>
-      </>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   )
 }
 
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// 함수 컴포넌트
+// function Header() {
+// Arrow 컴포넌트
+const Header = () => {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  )
+}
+
+export default Header
