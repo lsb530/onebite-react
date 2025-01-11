45c291c21e72f796c3d02c6a72acc025128086e2
boki <lsb530@naver.com>
2025-01-11 18:32:29 +0900

    props 전달 자식 props 전달, 부모->자식 단방향으로의 전달

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -15,7 +15,10 @@ function App() {
       {/*<Button text={"메일"} color={"red"}/>*/}
       <Button {...buttonProps}/>
       <Button text={"카페"}/>
-      <Button text={"블로그"}/>
+      <Button text={"블로그"}>
+        <div>자식요소</div>
+        <Header/>
+      </Button>
     </>
   )
 }
