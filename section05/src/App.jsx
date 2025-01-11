1dedcf0b83af51ce75b0ad4d2dbbdf31d79b90af
boki <lsb530@naver.com>
2025-01-11 19:35:02 +0900

    State & Props 관련없는 state를 다른 컴포넌트로 분리하여 불필요한 리렌더링 방지

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,38 +1,30 @@
 import './App.css'
 import { useState } from "react";
 
-const Bulb = ({ light }) => {
+const Bulb = () => {
+  const [light, setLight] = useState("OFF")
   // 자신이 갖는 state가 변경되지 않고 부모로부터 받는 props가 변경되면 리렌더링됨
   console.log(light)
-  return <div>{light === 'ON' ?
-    <h1 style={{
-      backgroundColor: 'orange'
-    }}>ON</h1> :
-    <h1 style={{
-      backgroundColor: 'gray'
-    }}>OFF</h1>}
-  </div>
+  return (
+    <div>{light === 'ON' ?
+      <h1 style={{
+        backgroundColor: 'orange'
+      }}>ON</h1> :
+      <h1 style={{
+        backgroundColor: 'gray'
+      }}>OFF</h1>}
+      <button onClick={() => setLight(light === 'ON' ? 'OFF' : 'ON')}>
+        {/*전구 끄기/켜기*/}
+        {light === 'ON' ? '끄기' : '켜기'}
+      </button>
+    </div>
+  )
 }
 
-function App() {
-  // const state = useState();
-  // console.log(state)
-  // 0: value(initial)
-  // 1: fn
-
-  // Re-rendering
+const Counter = () => {
   const [count, setCount] = useState(0)
-  const [light, setLight] = useState("OFF")
-
   return (
     <>
-      <div>
-        <Bulb light={light}/>
-        <button onClick={() => setLight(light === 'ON' ? 'OFF' : 'ON')}>
-          {/*전구 끄기/켜기*/}
-          {light === 'ON' ? '끄기' : '켜기'}
-        </button>
-      </div>
       <div>
         <h1>{count}</h1>
         <button onClick={() => {
@@ -41,6 +33,15 @@ function App() {
           +
         </button>
       </div>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Bulb/>
+      <Counter/>
     </>
   )
 }
