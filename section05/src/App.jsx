2a0de063711f9468439429f2288c42778f9ce0e4
boki <lsb530@naver.com>
2025-01-11 18:30:16 +0900

    props 전달 객체 props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -5,9 +5,15 @@ import Footer from "./components/Footer";
 import Button from "./components/Button";
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1, b: 2, c: 3
+  }
   return (
     <>
-      <Button text={"메일"} color={"red"}/>
+      {/*<Button text={"메일"} color={"red"}/>*/}
+      <Button {...buttonProps}/>
       <Button text={"카페"}/>
       <Button text={"블로그"}/>
     </>
