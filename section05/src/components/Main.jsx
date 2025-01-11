82bf26ecc56c836f34624b5910e4080bce8a9833
boki <lsb530@naver.com>
2025-01-11 18:11:15 +0900

    태그 스타일 차이

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -4,26 +4,26 @@ const Main = () => {
   // 2. 숫자, 문자열, 배열 값만 렌더링할 수 있음(객체 또한 그자체로 렌더링 안됨)
   // 3. 모든 태그는 닫혀있어야 한다
   // 4. 최상위 태그는 반드시 하나여야 한다(명시하지 않으면 spread component)
-  const number = 10
-  const obj = { a: 1 }
 
-  return (
-    // <div></div>
-    <main>
-      <h1>main</h1>
-      <h2>{number}</h2>
-      <h2>{number + 10}</h2>
-      <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
-      {/*/!*{ if() {} }*!/ 자바스크립트표현식이 아님*/}
-      {/*/!*{ for() {} }*!/ 위와 동일*/}
-      {[1, 2, 3]}
-      {true}
-      {undefined}
-      {null}
-      {/*{obj} 오류*/}
-      {obj.a}
-    </main>
-  )
+  const user = {
+    name: '이승복',
+    isLogin: false
+  }
+
+  if (user.isLogin) {
+    return <div>로그아웃</div>
+  } else {
+    return <div>로그인</div>
+  }
+
+  /*return (
+    <>
+      {user.isLogin ?
+        <div>로그아웃</div> :
+        <div>로그인</div>
+      }
+    </>
+  )*/
 }
 
 export default Main
