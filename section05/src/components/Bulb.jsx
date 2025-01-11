32234649e9353e1cdb4cfb1d2f8326227c8da970
boki <lsb530@naver.com>
2025-01-11 19:36:43 +0900

    State & Props 파일 분리로 모듈화

---
+++ section05/src/components/Bulb.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Bulb = () => {
+  const [light, setLight] = useState("OFF")
+  // 자신이 갖는 state가 변경되지 않고 부모로부터 받는 props가 변경되면 리렌더링됨
+  console.log(light)
+  return (
+    <div>{light === 'ON' ?
+      <h1 style={{
+        backgroundColor: 'orange'
+      }}>ON</h1> :
+      <h1 style={{
+        backgroundColor: 'gray'
+      }}>OFF</h1>}
+      <button onClick={() => setLight(light === 'ON' ? 'OFF' : 'ON')}>
+        {/*전구 끄기/켜기*/}
+        {light === 'ON' ? '끄기' : '켜기'}
+      </button>
+    </div>
+  )
+}
+
+export default Bulb
