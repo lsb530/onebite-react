0de4cce7c423fed7b6a9d9df8660003bb0023727
boki <lsb530@naver.com>
2025-01-12 16:58:52 +0900

    TodoList 앱 - TodoList 렌더링

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './List.css'
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState('')
+
+  const onChangeSearch = (e) => setSearch(e.target.value)
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos
+    }
+    return todos.filter((todo) =>
+      todo.content
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    )
+  }
+
+  const filteredTodos = getFilteredData()
+
   return (
     <div className="List">
       <h4>Todo List 🍀</h4>
-      <input placeholder="검색어를 입력하세요"/>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem key={todo.id} {...todo} />
+          )
+        })}
       </div>
     </div>
   );
