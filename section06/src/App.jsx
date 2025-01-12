5f47cba3388ee00a1fec02293572b83b4404ba60
boki <lsb530@naver.com>
2025-01-12 11:09:22 +0900

    카운터 앱 - 기능 구현 잘못된 예시: 형제 컴포넌트인 Viewer나 Controller 컴포넌트의 useState hook

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -3,6 +3,7 @@ import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
 
 function App() {
+
   return (
     <div className={"App"}>
       <h1>Simple Counter</h1>
--- section06/src/components/Viewer.jsx
+++ section06/src/components/Viewer.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Viewer = () => {
+  // const [count, setCount] = useState()
+
   return (
     <div>
       <div>현재 카운트 :</div>
+      {/*<h1>{count}</h1>*/}
       <h1>0</h1>
     </div>
   );
