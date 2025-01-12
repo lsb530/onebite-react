74b166b3bc27a19940fb03ddbe270ef4a759413b
boki <lsb530@naver.com>
2025-01-12 16:17:00 +0900

    TodoList 앱 - 초기 데이터 추가

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,8 +2,31 @@ import './App.css'
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import { useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    data: new Date().getTime()
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    data: new Date().getTime()
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    data: new Date().getTime()
+  }
+]
 
 function App() {
+  const [todos, setTodos] = useState(mockData)
   return (
     <div className="App">
       <Header/>
