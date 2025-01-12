aeb4669ec70c38a29906b29f36e42caa12f7b34f
boki <lsb530@naver.com>
2025-01-12 23:56:13 +0900

    최적화 useMemo를 사용하기 이전

---
+++ section10/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import React, { useRef, useState } from 'react';
+import './Editor.css'
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState('')
+  const contentRef = useRef()
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value)
+  }
+
+  const onKeyDown = (e) => {
+    const key = e.key || e.keyCode
+    if (key === 'Enter' && !e.shiftKey || key === 13 && key !== 16) {
+      onSubmit()
+    }
+  }
+
+  const onSubmit = () => {
+    if (content === '') {
+      contentRef.current.focus()
+      return
+    }
+    onCreate(content)
+    setContent('')
+  }
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
