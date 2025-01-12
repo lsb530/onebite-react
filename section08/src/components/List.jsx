c69af032ffefd68788f3a2a6fd49510a435237eb
boki <lsb530@naver.com>
2025-01-12 16:04:51 +0900

    TodoList 앱 - UI 구현

---
+++ section08/src/components/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './List.css'
+import TodoItem from "./TodoItem";
+
+const List = () => {
+  return (
+    <div className="List">
+      <h4>Todo List 🍀</h4>
+      <input placeholder="검색어를 입력하세요"/>
+      <div className="todos_wrapper">
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </div>
+    </div>
+  );
+};
+
+export default List;
