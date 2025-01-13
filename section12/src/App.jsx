adfb71cc844abf077a3c3ceaa62e9cb9f5a578b2
boki <lsb530@naver.com>
2025-01-13 23:12:49 +0900

    EmotionDiary 앱 - 폰트, 이미지, 레이아웃 설정하기 public/ vs assets/ 이미지 저장 차이

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,6 +5,12 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
 
+import emotion1 from "./assets/emotion1.png"
+import emotion2 from "./assets/emotion2.png"
+import emotion3 from "./assets/emotion3.png"
+import emotion4 from "./assets/emotion4.png"
+import emotion5 from "./assets/emotion5.png"
+
 // 1. "/": 모든 일기를 조회하는 Home 페이지
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
@@ -14,6 +20,26 @@ function App() {
 
   return (
     <>
+
+      <div>
+        {/*public에서 접근: 최적화 X*/}
+        <img src={"/emotion1.png"}/>
+        <img src={"/emotion2.png"}/>
+        <img src={"/emotion3.png"}/>
+        <img src={"/emotion4.png"}/>
+        <img src={"/emotion5.png"}/>
+      </div>
+
+
+      <div>
+        {/*assets에서 접근: 최적화 O(data URI)*/}
+        <img src={emotion1}/>
+        <img src={emotion2}/>
+        <img src={emotion3}/>
+        <img src={emotion4}/>
+        <img src={emotion5}/>
+      </div>
+
       <div>
         <Link to="/">Home</Link>
         <Link to="/new">New</Link>
