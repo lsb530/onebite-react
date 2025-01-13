31de76dc982f38a8b6ad19c0f2055a1282239010
boki <lsb530@naver.com>
2025-01-13 10:14:28 +0900

    EmotionDiary 앱 - 라우팅 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import NotFound from "./pages/NotFound";
 
+// 1. "/": 모든 일기를 조회하는 Home 페이지
+// 2. "/new": 새로운 일기를 작성하는 New 페이지
+// 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*<div>Hello</div> 경로와 상관없이 나오는 부분*/}
+      <Routes>
+        {/*<div>Routes 내부는 Route만 쓸 수 있다</div>*/}
+        <Route path="/" element={<Home/>}/>
+        <Route path="/new" element={<New/>}/>
+        <Route path="/diary" element={<Diary/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </>
   )
 }
