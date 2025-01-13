e9f3d19a679186ae5f2719fa667fd203a39caa18
boki <lsb530@naver.com>
2025-01-13 10:24:07 +0900

    EmotionDiary 앱 - 페이지 이동

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
@@ -9,9 +9,24 @@ import NotFound from "./pages/NotFound";
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const navigate = useNavigate();
+  const onClickButton = () => navigate("/new")
+
   return (
     <>
-      {/*<div>Hello</div> 경로와 상관없이 나오는 부분*/}
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/new">New</Link>
+        <Link to="/diary">Diary</Link>
+
+        {/*SPA 방식으로 작동 안함. blink(깜박임 발생)*/}
+        {/*<a href="/">Home</a>*/}
+        {/*<a href="/new">New</a>*/}
+        {/*<a href="/diary">Diary</a>*/}
+      </div>
+      <button onClick={onClickButton}>
+        New 페이지로 이동
+      </button>
       <Routes>
         {/*<div>Routes 내부는 Route만 쓸 수 있다</div>*/}
         <Route path="/" element={<Home/>}/>
