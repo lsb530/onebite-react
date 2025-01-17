a04267e3870fa23cdce4af99cdd443c367df2f22
boki <lsb530@naver.com>
2025-01-18 01:25:26 +0900

    EmotionDiary 앱 - Edit 페이지 params 훅 추가 및 코드정리

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -6,15 +6,10 @@ import Diary from './pages/Diary'
 import NotFound from './pages/NotFound'
 import Edit from './pages/Edit'
 
-// 1. "/": 모든 일기를 조회하는 Home 페이지
-// 2. "/new": 새로운 일기를 작성하는 New 페이지
-// 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
-// 4. "/edit": 일기를 수정하는 Edit 페이지
 function App() {
   return (
     <>
       <Routes>
-        {/*<div>Routes 내부는 Route만 쓸 수 있다</div>*/}
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Edit = () => {
+  const params = useParams()
   return (
     <div>
-      Edit
+      {params.id}번 일기 수정페이지입니다
     </div>
   )
 }
