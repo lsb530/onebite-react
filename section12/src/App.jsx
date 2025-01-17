6adb74e815098594e7e7310a45867e8a115d8030
boki <lsb530@naver.com>
2025-01-18 01:29:05 +0900

    EmotionDiary 앱 - 일기 관리 기능 구현1 useReducer mockData 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,8 +5,30 @@ import New from './pages/New'
 import Diary from './pages/Diary'
 import NotFound from './pages/NotFound'
 import Edit from './pages/Edit'
+import { useReducer } from 'react'
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용'
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용'
+  }
+]
+
+function reducer(state, action) {
+  return state
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData)
+
   return (
     <>
       <Routes>
