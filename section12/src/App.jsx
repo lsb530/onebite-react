6e3d2a413899b94918b15b08dcb8c43f887aa2bb
boki <lsb530@naver.com>
2025-01-18 01:23:16 +0900

    EmotionDiary 앱 - Edit 페이지 Route 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,52 +4,21 @@ import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
 import NotFound from './pages/NotFound'
-import Button from './components/Button'
-import Header from './components/Header'
+import Edit from './pages/Edit'
 
 // 1. "/": 모든 일기를 조회하는 Home 페이지
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
+// 4. "/edit": 일기를 수정하는 Edit 페이지
 function App() {
-  const navigate = useNavigate()
-  const onClickButton = () => navigate('/new')
-
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"}/> }
-        rightChild={<Button text={"Right"}/> }
-      />
-
-      <Button
-        text={'123'}
-        // type={'DEFAULT'}
-        onClick={() => {
-          console.log('123번 버튼 클릭')
-        }}
-      />
-
-      <Button
-        text={'123'}
-        type={'POSITIVE'}
-        onClick={() => {
-          console.log('123번 버튼 클릭')
-        }}
-      />
-
-      <Button
-        text={'123'}
-        type={'NEGATIVE'}
-        onClick={() => {
-          console.log('123번 버튼 클릭')
-        }}
-      />
       <Routes>
         {/*<div>Routes 내부는 Route만 쓸 수 있다</div>*/}
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
