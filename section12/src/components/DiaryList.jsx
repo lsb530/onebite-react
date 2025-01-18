c3a59f648bcd23962b073b50898a89cf40b7db11
boki <lsb530@naver.com>
2025-01-18 10:22:14 +0900

    EmotionDiary 앱 - Home 페이지 구현 2.기능 새 일기, 최신순/오래된 순 정렬 구현

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 import './DiaryList.css'
 import DiaryItem from './DiaryItem'
+import { useNavigate } from 'react-router-dom'
 
 const DiaryList = ({ data }) => {
+  const nav = useNavigate()
+  const [sortType, setSortType] = useState('latest')
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value)
+  }
+
+  const getSortedDataByCreatedDate = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === 'oldest') {
+        return Number(a.createdDate) - Number(b.createdDate)
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate)
+      }
+    })
+  }
+
+  const sortedData = getSortedDataByCreatedDate()
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={'latest'}>최신순</option>
           <option value={'oldest'}>오래된 순</option>
         </select>
-        <Button text={'새 일기 쓰기'} type={'POSITIVE'} />
+        <Button
+          onClick={() => nav('/new')}
+          text={'새 일기 쓰기'}
+          type={'POSITIVE'}
+        />
       </div>
       <div className="list_wrapper">
-        {data.map((item) => {
+        {sortedData.map((item) => {
           return (
             <DiaryItem
               key={item.id}
               {...item}
-              />
+            />
           )
         })}
       </div>
