f5f7d574f70ba7c8f2a371aa53bcef05a7db7def
boki <lsb530@naver.com>
2025-01-18 12:15:19 +0900

    EmotionDiary 앱 - New 페이지 구현 1.UI

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Editor from '../components/Editor'
 
 const New = () => {
   return (
     <div>
-      New
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로가기"}/>}
+      />
+      <Editor/>
     </div>
   )
 }
 
-export default New;
+export default New
